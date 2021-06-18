352af3862447cb8171f9147cc072def36c9d8290
yunusordek <yunusordek97@gmail.com>
2021-06-19 00:29:58 +0300

    Revert "elasticsearch added"
    This reverts commit 7dbe7a62

--- SpringExample/mongoRestApi/src/main/java/com/workSpace/Application.java
+++ SpringExample/mongoRestApi/src/main/java/com/workSpace/Application.java
@@ -9,5 +9,7 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+
+
     }
 }
