75ea1d5e8dc089090dded827194efebe14a86c54
yunusordek <yunusordek97@gmail.com>
2021-06-19 00:32:24 +0300

    Revert "Revert "elasticsearch added""
    This reverts commit 352af386

---
+++ SpringElasticsearch/src/main/java/com/WorkSpace/entity/Kisi.java
@@ -0,0 +1,71 @@
+package com.WorkSpace.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import java.util.Date;
+
+@Document(indexName = "kisiler")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Kisi {
+
+    @Id
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAd() {
+        return ad;
+    }
+
+    public void setAd(String ad) {
+        this.ad = ad;
+    }
+
+    public String getSoyad() {
+        return soyad;
+    }
+
+    public void setSoyad(String soyad) {
+        this.soyad = soyad;
+    }
+
+    public String getAdres() {
+        return adres;
+    }
+
+    public void setAdres(String adres) {
+        this.adres = adres;
+    }
+
+    public Date getDogumTarihi() {
+        return dogumTarihi;
+    }
+
+    public void setDogumTarihi(Date dogumTarihi) {
+        this.dogumTarihi = dogumTarihi;
+    }
+
+    @Field(name = "ad", type = FieldType.Text)
+    private String ad;
+
+    @Field(name = "soyad", type = FieldType.Text)
+    private String soyad;
+
+    @Field(name = "adres", type = FieldType.Text)
+    private String adres;
+
+    @Field(name = "dogumTarihi", type = FieldType.Date)
+    private Date dogumTarihi;
+}
